e682d0ca7f606d5f0588705958b39991f397e102
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-01 17:58:45 +0200

    Update ProductCard component layout, add CSS styles, and include Google Fonts

--- src/componets/ProductCard.jsx
+++ src/componets/ProductCard.jsx
@@ -1,6 +1,15 @@
 function ProductCard() {
     return (
-        <h1>sono una card</h1>
+        <div className="container d-flex justify-content-center mt-5">
+            <div className="card card col-4 border-black rounded-3 shadow">
+                <img src="../src/assets/ghost.png" className="card-img-top" alt="ghost-of-tsushima" />
+                <div className="card-body">
+                    <h4 className="card-title">Ghost of Tsushima</h4>
+                    <p className="card-text pb-2">Ghost of Tsushima è un videogioco d'avventura dinamica e stealth sviluppato da Sucker Punch Productions.</p>
+                    <button className="btn mt-4 fw-bold">SCOPRI</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
